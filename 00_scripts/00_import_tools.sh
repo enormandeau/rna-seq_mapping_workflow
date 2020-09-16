5ab0467008860334b64bba8a410b41d9818199b4
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-09-16 14:26:49 -0400

    Mod to work on Asgaard

--- 00_scripts/00_import_tools.sh
+++ 00_scripts/00_import_tools.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#pre-requis
-module load apps/git/1.8.5.3
+##pre-requis
+#module load apps/git/1.8.5.3
 
-#move to current directory
+##move to current directory
 
-cd $(pwd)
+#cd $(pwd)
 
+#clone into trinity
+git clone https://github.com/trinityrnaseq/trinityrnaseq 00_scripts/trinity_utils
 
 # clone trinotate
 git clone https://github.com/Trinotate/Trinotate 00_scripts/trinotate_utils
--- 00_scripts/01_trimmomatic_pe.sh
+++ 00_scripts/01_trimmomatic_pe.sh
@@ -3,13 +3,12 @@
 #PBS -o trimmomatic__BASE__.out
 #PBS -l walltime=02:00:00
 #PBS -l mem=60g
-#####PBS -m ea 
+#####PBS -m ea
 #PBS -l ncpus=8
 #PBS -q omp
 #PBS -r n
 
-cd $PBS_O_WORKDIR
-
+#cd $PBS_O_WORKDIR
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
@@ -17,24 +16,27 @@ NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
-
 # Global variables
-
 ADAPTERFILE="univec.fasta"
-NCPU=8
-base=__BASE__
+TRIMMOMATIC_JAR="trimmomatic-0.36.jar"
+NCPU=20
 
-trimmomatic PE -Xmx60G \
-        -threads $NCPU \
-        -phred33 \
-        02_data/"$base"_R1.fastq.gz \
-        02_data/"$base"_R2.fastq.gz \
-        03_trimmed/"$base"_R1.paired.fastq.gz \
-        03_trimmed/"$base"_R1.single.fastq.gz \
-        03_trimmed/"$base"_R2.paired.fastq.gz \
-        03_trimmed/"$base"_R2.single.fastq.gz \
-        ILLUMINACLIP:"$ADAPTERFILE":2:20:7 \
-        LEADING:20 \
-        TRAILING:20 \
-        SLIDINGWINDOW:30:30 \
-        MINLEN:60 2> 98_log_files/log.trimmomatic.pe."$TIMESTAMP"   
+for i in $(ls -1 02_data/NS*_R1.fastq.gz | perl -pe 's/_R1\.fastq\.gz//')
+do
+    base=$(basename "$i")
+    echo "Treating $base"
+        java -Xmx60G -jar $TRIMMOMATIC_JAR PE \
+            -threads $NCPU \
+            -phred33 \
+            02_data/"$base"_R1.fastq.gz \
+            02_data/"$base"_R2.fastq.gz \
+            03_trimmed/"$base"_R1.paired.fastq.gz \
+            03_trimmed/"$base"_R1.single.fastq.gz \
+            03_trimmed/"$base"_R2.paired.fastq.gz \
+            03_trimmed/"$base"_R2.single.fastq.gz \
+            ILLUMINACLIP:"$ADAPTERFILE":2:20:7 \
+            LEADING:20 \
+            TRAILING:20 \
+            SLIDINGWINDOW:30:30 \
+            MINLEN:60 2> 98_log_files/log.trimmomatic.pe."$TIMESTAMP"
+done
